b6347689fce6f236f8813117434ef4333f128332
lnalmeida <l.n.almeida.ti@gmail.com>
2022-05-15 20:34:53 -0300

    Mobile app initiated 30m/class4

---
+++ mobile/src/components/Widget/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+
+import { styles } from './styles.ts';
+
+export function Widget() {
+  return (
+    <>
+        <TouchableOpacity>
+
+
+
+        </TouchableOpacity>  
+    </>
+  );
+}
